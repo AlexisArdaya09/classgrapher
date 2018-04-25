6082f7881a2397d7fad8004164ede1480a837a06
wilson crespo <wilsoncrespo@gmail.com>
2018-04-25 05:46:24 -0400

    exp/BB-50: implementation for memento design pattern was added

--- src/entities/memento/Memento.java
+++ src/entities/memento/Memento.java
@@ -4,4 +4,17 @@ package entities.memento;
  * Created by Wilson Rolando Crespo Omonte on 25/04/2018.
  */
 public class Memento {
+    private Object state;
+
+    public Memento(Object state) {
+        this.state = state;
+    }
+
+    public Object getState() {
+        return state;
+    }
+
+    public void setState(Object state) {
+        this.state = state;
+    }
 }
--- src/entities/memento/Originator.java
+++ src/entities/memento/Originator.java
@@ -3,5 +3,7 @@ package entities.memento;
 /**
  * Created by Wilson Rolando Crespo Omonte on 25/04/2018.
  */
-public class Originator {
+public interface Originator {
+    Memento getMemento();
+    void setMemento(Memento memento);
 }

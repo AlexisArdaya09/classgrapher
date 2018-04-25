18bde75bb1bd4f24e69056679a1770e39a979d4d
wilson crespo <wilsoncrespo@gmail.com>
2018-04-25 05:46:24 -0400

    exp/BB-50: implementation for memento design pattern was added

--- src/entities/memento/CareTaker.java
+++ src/entities/memento/CareTaker.java
@@ -1,7 +1,24 @@
 package entities.memento;
 
+import java.util.Vector;
+
 /**
  * Created by Wilson Rolando Crespo Omonte on 25/04/2018.
  */
 public class CareTaker {
+    private Vector<Memento> statuses;
+    private int iterator;
+
+    public CareTaker() {
+        statuses = new Vector<>();
+        iterator = -1;
+    }
+
+    public Memento undo() {
+        return iterator > 0 ? statuses.get(--iterator) : null;
+    }
+
+    public Memento redo() {
+        return iterator < (statuses.size() - 1) ? statuses.get(++iterator) : null;
+    }
 }

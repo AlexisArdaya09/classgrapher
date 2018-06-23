89ebb59e547cc56d9d60ca1abdc5f492186ed9d5
Liyina Veizaga <liyina.veizaga@swissbytes.ch>
2018-06-23 16:23:09 -0400

    [unit_test] testAddOneConnectorToLogicBoard

--- test/core/LogicBoardTest.java
+++ test/core/LogicBoardTest.java
@@ -1,5 +1,8 @@
 package core;
 
+import core.exception.ConnectorException;
+import entities.classes.NormalClass;
+import entities.relations.NormalRelation;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,7 +10,15 @@ import static org.junit.Assert.*;
 public class LogicBoardTest {
 
     @Test
-    public void addConnector() {
+    public void testAddOneConnectorToLogicBoard() throws ConnectorException {
+        NormalClass classA = new NormalClass("main", new Point(0,0), new Point(20,20));
+
+        NormalClass classB = new NormalClass("two", new Point(30,30), new Point(50,50));
+        NormalRelation normalRelation = new NormalRelation(new Point(20,20), new Point(30,30));
+        Connector connector = new Connector(classA, classB, normalRelation);
+        LogicBoard logicBoard = new LogicBoard();
+        logicBoard.addConnector(connector);
+        assertEquals(1,logicBoard.connectors.size());
     }
 
     @Test

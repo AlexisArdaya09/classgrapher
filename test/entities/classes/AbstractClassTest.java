501fdc4d00c28ca378de681d973ec559fe63c72b
Liyina Veizaga <liyina.veizaga@swissbytes.ch>
2018-06-23 16:55:40 -0400

    [unit_test] Add skeleton AbstractClassTest

---
+++ test/entities/classes/AbstractClassTest.java
@@ -0,0 +1,16 @@
+package entities.classes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AbstractClassTest {
+
+    @Test
+    public void draw() {
+    }
+
+    @Test
+    public void getShapeInCoordinate() {
+    }
+}

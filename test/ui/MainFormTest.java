020f1545ad8ec7fb86b678b47dc30982d0afbdfa
Gabriel Morales <gabriel.morales@swissbytes.ch>
2018-06-23 18:17:21 -0400

    [unit_test] Add skeleton for test

---
+++ test/ui/MainFormTest.java
@@ -0,0 +1,16 @@
+package ui;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainFormTest {
+
+    @Test
+    public void saveFile() {
+    }
+
+    @Test
+    public void openFileChooser() {
+    }
+}

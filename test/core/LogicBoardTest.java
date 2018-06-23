0c5e13b87608cc473739860eeb7820089c7fd461
Liyina Veizaga <liyina.veizaga@swissbytes.ch>
2018-06-23 16:10:31 -0400

    [unit_test] Add skeleton unit_test LogicBoard

---
+++ test/core/LogicBoardTest.java
@@ -0,0 +1,24 @@
+package core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LogicBoardTest {
+
+    @Test
+    public void addConnector() {
+    }
+
+    @Test
+    public void selectShape() {
+    }
+
+    @Test
+    public void unSelectShape() {
+    }
+
+    @Test
+    public void removeConnectorsFromSelectedShape() {
+    }
+}
